a132e23792cc83b765c0bab7fd78f30f2653b5fd
Raul Escobar <raul3@microanalisis.com>
2023-12-14 08:59:45 -0600

    darwin macbook config

---
+++ hosts/macbook/configuration.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ <home-manager/nix-darwin> ];
+
+  users.users.raulescobar = {
+    name = "raulescobar";
+    home = "/Users/raulescobar";
+  };
+  
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [ 
+    ];
+  environment.pathsToLink = [ "/share/zsh" ];
+  nixpkgs.config.allowUnfree = true;
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  nix.package = pkgs.nix;
+  settings.experimental-features = [ "nix-command" "flakes" ];
+  programs.zsh.enable = true;  # default shell on catalina
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
