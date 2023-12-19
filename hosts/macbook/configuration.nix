28d039a290cffc22cff182d552b5011355a895c4
Raul Escobar <raul3@microanalisis.com>
2023-12-19 12:22:33 -0600

    yabai

--- hosts/macbook/configuration.nix
+++ hosts/macbook/configuration.nix
@@ -17,6 +17,28 @@
   users.users.raulescobar = {
     name = "raulescobar";
     home = "/Users/raulescobar";
+    description = "Raul Escobar Garcia";
+    shell = pkgs.zsh;
+  };
+
+  time = {
+    timeZone = "America/Chicago";
+  };
+
+  system = {
+    defaults = {
+      finder = {
+        CreateDesktop = false;
+      };
+      dock = {
+        show-recents = false;
+        autohide = true;
+        autohide-delay = 0.24;
+        orientation = "bottom";
+        tilesize = 32;
+        static-only = true;
+      };
+    };
   };
 
   
@@ -26,6 +48,26 @@
     spotify
     prismlauncher
   ];
+
+  services = {
+    yabai = {
+      enable = true;
+      enableScriptingAddition = true;
+      config = {
+        mouse_follows_focus = false;
+        focus_follows_mouse = true;
+        window_shadow = true;
+        window_opacity = true;
+        mouse_modifier = "cmd";
+        top_padding = 10;
+        bottom_padding = 10;
+        right_padding = 10;
+        left_padding = 10;
+        window_gap = 10;
+        layout = "bsp";
+      };
+    };
+  };
 
   environment.pathsToLink = [ "/share/zsh" ];
   nixpkgs.config.allowUnfree = true;
--- modules/macbook/home-manager.nix
+++ modules/macbook/home-manager.nix
@@ -45,6 +45,9 @@ in
     kitty = {
       enable = true;
       shellIntegration.enableZshIntegration = true;
+      darwinLaunchOptions = [
+        "-o allow_remote_control=yes" 
+      ];
       environment = { "KITTY_ENABLE_WAYLAND"="1"; };
       font = {
         name = "Berkeley Mono";
@@ -54,6 +57,9 @@ in
         "ctrl+c" = "copy_or_interrupt";
       };
       settings = {
+        allow_remote_control = true;
+        macos_traditional_fullscreen = true;
+        dynamic_background_opacity = true;
         scrollback_lines = 10000;
         enable_audio_bell = false;
         update_check_interval = 0;
@@ -174,14 +180,17 @@ in
     zellij = {
       enable = true;
       enableZshIntegration = true;
-      settings = {
-        theme = "custom";
-        themes.custom.fg = "#ffffff";
-        themes.custom.bg = "#161616";
-      };
+      # settings = {
+      #   theme = "custom";
+      #   themes.custom.fg = "#ffffff";
+      #   themes.custom.bg = "#161616";
+      # };
     };
     fzf = {
       enable = true;
+      enableZshIntegration = true;
+      colors = {};
+      # many more options can go here
     };
     ripgrep = {
       enable = true;
