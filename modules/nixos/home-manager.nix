ea153ca3e2bd1a66147ef7bf43da0e4714785b4d
Raul Escobar <raul3@microanalisis.com>
2023-12-19 23:11:37 -0600

    idek

--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -6,7 +6,7 @@ in
   home.username = "raulescobar_g";
   home.homeDirectory = "/home/raulescobar_g";
   home.stateVersion = "23.11"; # Please read the comment before changing.
-  
+  # hi  
   wayland = {
     windowManager = {
       hyprland = {
@@ -160,8 +160,7 @@ in
 	  bind = $mainMod, mouse_down, workspace, e+1
 	  bind = $mainMod, mouse_up, workspace, e-1
 	  
-	  # Move/resize windows with mainM:w
-	  # od + LMB/RMB and dragging
+	  # Move/resize windows with mainM:wod + LMB/RMB and dragging
 	  bindm = $mainMod, mouse:272, movewindow
  	  bindm = $mainMod, mouse:273, resizewindow	    
 	'';
