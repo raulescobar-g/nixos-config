6baeb6558407b0fbfd6ba4f57e2f67ffde418e3f
Raul Escobar <raul3@microanalisis.com>
2024-01-05 19:14:34 -0600

    wip

--- modules/macbook/home-manager.nix
+++ modules/macbook/home-manager.nix
@@ -2,7 +2,7 @@
 let 
   berkeley-mono = pkgs.callPackage ../font/berkeley-mono.nix {};
   wallpapa = pkgs.writeShellScriptBin "wallpapa" ''
-      #!/usr/bin/env bash 
+      #!/usr/bin/env zsh
       WALLPAPER_DIR="$HOME/Wallpapers"
       RANDOM_WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
       /usr/bin/osascript -e "
@@ -19,16 +19,11 @@ in
   home.stateVersion = "23.11"; # Please read the comment before changing.
   
   fonts.fontconfig.enable = true;
-  home.activation = {
-    wallpapa = lib.hm.dag.entryAfter["writeBoundary"] ''
-      ${wallpapa}/bin/wallpapa
-    '';
-  };
   
-
   home.packages = with pkgs; [
     berkeley-mono
     pfetch
+    uwufetch
     wallpapa
     skhd
     (nerdfonts.override { fonts = [ "Hack" ]; })
@@ -45,42 +40,61 @@ in
       enable = true;
       executable = true;
       text = ''
-        #!/usr/bin/env bash
-        sketchybar --set $NAME label="$(date '+%I%M  %m%d')"
+        #!/usr/bin/env zsh
+        sketchybar --set $NAME label="$(date '+%b%d %I%M')"
       '';
     };
     ".config/sketchybar/plugins/desktops.sh" = {
       enable = true;
       executable = true;
       text = ''
-        #!/usr/bin/env bash
-        DATA="$(yabai -m query --spaces --space)"
-        DESKTOP="$(DATA | jq '.index')"
-        sketchybar --set $NAME label="$DESKTOP"
+        #!/usr/bin/env zsh
+        sketchybar --set $NAME label="$(yabai -m query --spaces --space | jq '.index')"
       '';
     };
     "/.config/sketchybar/plugins/application.sh" = {
       enable = true;
       executable = true;
       text = ''
-        #!/usr/bin/env bash
+        #!/usr/bin/env zsh 
         APP=$(osascript -e 'tell application "System Events" to get name of application processes whose frontmost is true and visible is true')
 
         sketchybar --set $NAME label=$APP
+      '';
+    };
+    ".config/sketchybar/plugins/cpu.sh" = {
+      enable = true;
+      executable = true;
+      text = ''
+        #!/usr/bin/env zsh
+        CPU=$(top -l  2 | grep "^CPU" | tail -1 | awk '{ print int($3 + $5)"c" }')
+        sketchybar --set $NAME label="$CPU"
+      '';
+    };
+    ".config/sketchybar/plugins/mem.sh" = {
+      enable = true;
+      executable = true;
+      text = ''
+        #!/usr/bin/env zsh
+        MEM=$(top -l  2 | grep "^PhysMem" | tail -1 | awk '{ print $2 }')
+        sketchybar --set $NAME label="$MEM"
+      '';
+    };
+    ".config/sketchybar/plugins/volume.sh" = {
+      enable = true;
+      executable = true;
+      text = ''
+        #!/usr/bin/env zsh
+        VOLUME=$(osascript -e 'set ovol to output volume of (get volume settings)' | awk '{ print $1"vl" }')
+        sketchybar --set $NAME label="$VOLUME"
       '';
     };
     ".config/sketchybar/plugins/battery.sh" = {
       enable = true;
       executable = true;
       text = ''
-        #!/usr/bin/env bash
-        PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
-        CHARGING=$(pmset -g batt | grep 'AC Power')
-
-        if [ $PERCENTAGE = "" ]; then
-          exit 0
-        fi
-
+        #!/usr/bin/env zsh
+        PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1 | awk '{ print $1"bt" }')
         sketchybar --set $NAME label="$PERCENTAGE"
       '';
     };
@@ -94,76 +108,74 @@ in
         BG_HEIGHT=$((BAR_HEIGHT-BG_PADDING))
 
         sketchybar --bar \
-          color=0x00000000 \
+          color=0xff161616 \
           shadow=off \
-          y_offset=3 \
+          y_offset=0 \
           corner_radius=0 \
-          padding_left=10 \
-          padding_right=10 \
+          padding_left=0 \
+          padding_right=0 \
           notch_width=190 \
           margin=0 \
           height=$BAR_HEIGHT \
           display=all 
         
         sketchybar --default \
-          label.font="Berkeley Mono:Bold:20.0" \
-          label.color=0xffffffff \
+          label.font="Berkeley Mono:Regular:20.0" \
+          label.color=0xffdde1e6 \
           label.padding_right=10 \
           label.padding_left=10 \
-          icon.font="Hack Nerd Font:Bold:20.0" \
-          icon.color=0xffffffff \
-          icon.padding_left=0 \
-          icon.padding_right=0 \
+          icon.font="Hack Nerd Font:Regular:20.0" \
           padding_left=10 \
           padding_right=10 \
-          background.height=$BG_HEIGHT \
-          background.color=0xff393939 \
-          background.corner_radius=4 
 
         sketchybar --add item time right \
           --set time \
-            label.padding_right=12 \
-            label.padding_left=12 \
-            icon.padding_left=0 \
-            icon.padding_right=0 \
             update_freq=5 \
             script="$PLUGINS/datetime.sh"
+
+        sketchybar --add item separator.1 right \
+          --set separator.1 \
+            padding_left=2 \
+            padding_right=2 \
+            label="|"
 
         sketchybar --add item battery right \
           --set battery \
             script="$PLUGINS/battery.sh" \
             update_freq=10 \
-            icon.y_offset=1 \
           --subscribe battery system_woke power_source_change 
+
+        sketchybar --add item volume right \
+          --set volume \
+            script="$PLUGINS/volume.sh" \
+          --subscribe volume volume_change
         
-        sketchybar --add bracket sysinfo battery time \
-          --set sysinfo background.color=0xff161616 \
-            background.corner_radius=6 \
-            background.height=$BAR_HEIGHT \
-            background.border_color=0xffffffff \
-            background.border_width=0 \
-            background.y_offset=0 \
-            drawing=on 
-        
+        sketchybar --add item separator.2 right \
+          --set separator.2 \
+            padding_left=2 \
+            padding_right=2 \
+            label="|"
+
+        sketchybar --add item cpu right \
+          --set cpu \
+            script="$PLUGINS/cpu.sh" \
+            update_freq=10
+
+        sketchybar --add item mem right \
+          --set mem \
+            script="$PLUGINS/mem.sh" \
+            update_freq=10
+
         sketchybar --add item desktops left \
           --set desktops \
             script="$PLUGINS/desktops.sh" \
-            update_freq=1 \
           --subscribe desktops space_change display_change
 
-        sketchybar --add item application q \
-          --set application script="$PLUGINS/application.sh" \
+        sketchybar --add item application left \
+          --set application \
+            script="$PLUGINS/application.sh" \
           --subscribe application front_app_switched space_change space_windows_change display_change
-          
-        sketchybar --add bracket userinfo desktops application \
-          --set userinfo background.color=0xff161616 \
-            background.height=$BAR_HEIGHT \
-            background.border_color=0xffffffff \
-            background.border_width=0 \
-            background.corner_radius=6 \
-            background.y_offset=0 \
-            drawing=on 
-         
+
         sketchybar --update
       '';
     };
@@ -175,7 +187,7 @@ in
         ctrl - k: yabai -m window --focus north
         ctrl - h: yabai -m window --focus west
         ctrl - l: yabai -m window --focus east
-        cmd - l: yabai -m space --focus next
+        cmd - l: yabai -m space --focus next 
         cmd - h: yabai -m space --focus prev 
         cmd - m: yabai -m window --toggle zoom-fullscreen
 
@@ -185,7 +197,8 @@ in
       enable = true;
       executable = true;
       text = ''
-        borders active_color="glow(0xff3ddbd9)" inactive_color=0xff525252 width=4.0 &
+        wallpapa
+        # borders active_color="glow(0xffffffff)" inactive_color=0xff525252 width=1.0 &
 
         yabai -m config mouse_follows_focus off 
         yabai -m config focus_follows_mouse autofocus 
@@ -218,7 +231,7 @@ in
     sf = "fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}' | xargs nvim";
     code = "nvim";
     grep = "rg";
-    pfetch = "PF_ASCII=\"Linux\" pfetch";
+    pfetch = "PF_INFO=\"ascii title os host kernel uptime pkgs memory shell editor wm de palette\" PF_COL1=1 PF_COL2=8 PF_COL3=6 PF_ASCII=\"Linux\" pfetch";
     clear = "clear && pfetch";
     suramba = "sudo rm -rf /etc/bashrc /etc/zshenv /etc/zshrc";
   };
@@ -233,7 +246,7 @@ in
       enableCompletion = true; # move wallpaper setting to system wide init
       initExtra = ''
         eval "$(starship init zsh)"
-        PF_ASCII="Linux" pfetch
+        PF_INFO="ascii title os host kernel uptime pkgs memory shell editor wm de palette" PF_COL1=1 PF_COL2=8 PF_COL3=6 PF_ASCII="Linux" pfetch
       '';
     };
     neovim = {
@@ -323,13 +336,13 @@ in
           style = "bold bright-black";
         };
         directory = {
-          style = "bold #be95ff";
+          style = "bold bright-black";
           truncation_length = 5;
-          format = "[$path]($style)[$lock_symbol]($lock_style) ";
-          disabled = true; 
+          format = "∃ [$path]($style)[$lock_symbol]($lock_style) ";
+          disabled = false; 
         };
         git_branch = {
-          format = "∃ [$branch]($style)";
+          format = "∊ [$branch]($style)";
           style = "bold #be95ff";
         };
         git_commit ={
